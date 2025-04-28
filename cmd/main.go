d828925056bfd2a87f8458d47070c3b293ee18f7
Кот-кайфун Новый <Vladimir_225meil.ru@mail.ru>
2025-04-28 22:23:53 +0300

    + Add some piece of architechture to this proj

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 )
 
-func main() {
-	fmt.Println("We start to play) Welcome!")
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	w.Write([]byte("Home page of Messengere"))
 }
 
-// I will try to do this project by tdd
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Отображение заметки..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Форма для создания новой замтки.."))
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
+
+	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}

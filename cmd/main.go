8686be35b77fb1cf8e66bd30e99947d9491a3d87
Кот-кайфун Новый <Vladimir_225meil.ru@mail.ru>
2025-05-04 19:43:59 +0300

    Переработал homepagehandler добавил пару страниц и почистил main

--- cmd/main.go
+++ cmd/main.go
@@ -1,39 +1,19 @@
 package main
 
 import (
+	services "Chatmod/internal/services"
 	"log"
 	"net/http"
-	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	// Проверяется, если текущий путь URL запроса точно совпадает с шаблоном "/". Если нет, вызывается
-	// функция http.NotFound() для возвращения клиенту ошибки 404.
-	// Важно, чтобы мы завершили работу обработчика через return. Если мы забудем про "return", то обработчик
-	// продолжит работу и выведет сообщение "Привет из SnippetBox" как ни в чем не бывало.
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-}
-
-// Обработчик для отображения содержимого заметки.
-func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Отображение заметки..."))
-}
-
-// Обработчик для создания новой заметки.
-func createSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Форма для создания новой замтки.."))
-}
+// Нужны два новых обработчика для страниц: loginPage, registrationPage
+// Need two new handlers for pages: loginPage, registrationPage
 
 func main() {
 	// Регистрируем два новых обработчика и соответствующие URL-шаблоны в
 	// маршрутизаторе servemux
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", homePageHandler)
-	mux.HandleFunc("/snippet", showSnippet)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/", services.HomePageHandler)
 
 	// Adding css styles handler from file assets
 	fileServer := http.FileServer(http.Dir("../ui/templates/assets"))
@@ -45,39 +25,4 @@ func main() {
 }
 
 // New function home. Need to replace it to top.
-// Handler for home page
-func homePageHandler(w http.ResponseWriter, r *http.Request) {
-	// Проверяется, если текущий путь URL запроса точно совпадает с шаблоном "/". Если нет, вызывается
-	// функция http.NotFound() для возвращения клиенту ошибки 404.
-	// Важно, чтобы мы завершили работу обработчика через return. Если мы забудем про "return", то обработчик
-	// продолжит работу и выведет сообщение "Привет из SnippetBox" как ни в чем не бывало.
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	// Create simple info about page
-	data := struct {
-		PageName string
-		Content  string
-	}{
-		PageName: "Home",
-		Content:  "Some Information about ass",
-	}
-
-	// Create adresses to html files. Home files and layout
-	tmpl, err := template.ParseFiles("../ui/templates/layout.html",
-		"../ui/templates/homePage.html") // error system can't fine path home.html
-	if err != nil {
-		log.Printf("homePageHandler error with file parsing: %v", err)
-		return
-	}
-
-	// Send data to page
-	err = tmpl.Execute(w, data)
-	if err != nil {
-		log.Printf("homePageHandler error with file executing: %v", err)
-	}
-
-}
-
 // Прикольно было бы добавить автоматическое открытие сайта при запуске программы
